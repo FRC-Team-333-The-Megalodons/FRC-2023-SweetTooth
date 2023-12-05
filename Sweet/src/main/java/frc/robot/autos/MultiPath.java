b0f6d61170708dac5717e4e87bb2371487005a4c
Emmanuel <ejeffers22@gmail.com>
2023-12-05 00:26:51 -0500

    Added Multi Game piece auto using PathPlanning. Need to test

--- Sweet/src/main/java/frc/robot/autos/MultiPath.java
+++ Sweet/src/main/java/frc/robot/autos/MultiPath.java
@@ -1,11 +1,8 @@
-package frc.robot.autos;
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-import frc.robot.Constants;
-import frc.robot.commands.GoIntake;
-import frc.robot.commands.IntakeCube;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Pivot;
-import frc.robot.subsystems.Swerve;
+package frc.robot.autos;
 
 import java.util.HashMap;
 
@@ -17,44 +14,48 @@ import com.pathplanner.lib.commands.FollowPathWithEvents;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-
+import frc.robot.Constants;
+import frc.robot.Constants.Intake.IntakeConstants;
+import frc.robot.commands.EjectCube;
+import frc.robot.commands.IntakeCube;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Swerve;
 public class MultiPath extends SequentialCommandGroup {
-    public MultiPath(Swerve swerve, Pivot pivot, Intake intake){
+  /** Creates a new MultiPath. */
+  public MultiPath(Swerve swerve, Intake intake) {
 
-        PathPlannerTrajectory examplePath = PathPlanner.loadPath("Multi Path", new PathConstraints(4, 3));
+    PathPlannerTrajectory examplePath = PathPlanner.loadPath("Forward Backward", new PathConstraints(4, 3));
 
-        HashMap<String, Command> eventMap = new HashMap<>();
-        eventMap.put("marker1", new PrintCommand("Passed Marker 1"));
-        eventMap.put("Intake Down", new GoIntake(pivot).alongWith(new IntakeCube(intake).until(intake::intakeAutoDone).withTimeout(1)));
+    HashMap<String, Command> eventMap = new HashMap<>();
+    eventMap.put("marker1", new PrintCommand("Passed Marker 1"));
+    eventMap.put("Eject Cube", new EjectCube(intake, IntakeConstants.midtakeSpeed).until(intake::outakeAutoDone));
+    eventMap.put("Intake Cube", new IntakeCube(intake).until(intake::intakeAutoDone));
 
-        var thetaController =
-            new ProfiledPIDController(
-                Constants.AutoConstants.kPThetaController, 0, 0, Constants.AutoConstants.kThetaControllerConstraints);
-        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    var thetaController =
+    new ProfiledPIDController(
+        Constants.AutoConstants.kPThetaController, 0, 0, Constants.AutoConstants.kThetaControllerConstraints);
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-        SwerveControllerCommand swerveControllerCommand =
-            new SwerveControllerCommand(
-                examplePath,
-                swerve::getPose,
-                Constants.SwerveDrive.swerveKinematics,
-                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
-                new PIDController(Constants.AutoConstants.kPYController, 0, 0),
-                thetaController,
-                swerve::setModuleStates,
-                swerve);
+    SwerveControllerCommand swerveControllerCommand =
+        new SwerveControllerCommand(
+            examplePath,
+            swerve::getPose,
+            Constants.SwerveDrive.swerveKinematics,
+            new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+            new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+            thetaController,
+            swerve::setModuleStates,
+            swerve);
+
+    FollowPathWithEvents command = new FollowPathWithEvents(
+      swerveControllerCommand, 
+      examplePath.getMarkers(), 
+      eventMap
+    );
         
-        FollowPathWithEvents command = new FollowPathWithEvents(
-            (Command) examplePath, 
-            examplePath.getMarkers(), 
-            eventMap
-        );
-
-
-        addCommands(
-        );
-    }
+    addCommands(command);
+  }
 }
